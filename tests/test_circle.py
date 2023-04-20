b299d4f021672910d774c9826d864c9790bff612
snikiforova <d6796055@gmail.com>
2023-04-21 00:24:56 +0300

    тесты

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -13,7 +13,8 @@ def test_circle_creation_positive(radius, expected_area, expected_perimeter):
     assert circle.get_area() == expected_area, 'Expected correct area'
     assert circle.get_perimeter() == expected_perimeter, 'Expected correct perimeter'
 
-    @pytest.mark.parametrize('radius',
+
+@pytest.mark.parametrize('radius',
                              [
                                  0,
                                  -2,
@@ -22,19 +23,13 @@ def test_circle_creation_positive(radius, expected_area, expected_perimeter):
                                  'radius is zero',
                                  'radius is negative',
                              ])
-    def test_circle_creation_negative(radius):
+def test_circle_creation_negative(radius):
         with pytest.raises(ValueError):
             Circle(radius)
 
-            def test_two_circle_areas_sum():
-                expected_sum = 30
-                circle_1 = Circle(10)
-                circle_2 = Circle(2)
-                assert circle_1.add_area(circle_2) == expected_sum, f'Expected sum is {expected_sum}'
 
-@pytest.mark.parametrize('radius', [-10, 0, -50.5], ids=['negative', 'zero', 'negative_float'])
-def test_circle_creation_negative(radius):
-    with pytest.raises(ValueError):
-        Circle(radius)
-
-
+def test_two_circle_areas_sum():
+    expected_sum = 326.56
+    circle_1 = Circle(10)
+    circle_2 = Circle(2)
+    assert circle_1.add_area(circle_2) == expected_sum, f'Expected sum is {expected_sum}'
