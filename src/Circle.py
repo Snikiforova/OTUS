177f27941d1a20c5aff4c4b10ba2215141a4857a
snikiforova <d6796055@gmail.com>
2023-04-20 13:37:36 +0300

    тесты

--- src/Circle.py
+++ src/Circle.py
@@ -1,14 +1,17 @@
+
 from src.Figure import Figure
+
 class Circle(Figure):
-  def __init__(self, radius):
-    super().init(name='Circle')
-    self.radius = radius
-    if self.radius <= 0:
-        print("Радиус должен быть больше 0!")
-        raise ValueError
 
-  def get_area(self):
-    return 3.14 * self.radius ** 2
+    def __init__(self, radius):
+        super().__init__('Circle')
+        self.radius = radius
+        if self.radius <= 0:
+            print("Радиус должен быть больше 0!")
+            raise ValueError
 
-  def get_perimeter(self):
-    return 2 * 3.14 * self.radius
+    def get_area(self):
+        return 3.14 * self.radius ** 2
+
+    def get_perimeter(self):
+        return 2 * 3.14 * self.radius
---
+++ tests/test_circle.py
@@ -0,0 +1,40 @@
+import pytest
+from src.Circle import Circle
+
+
+@pytest.mark.parametrize('radius, expected_area, expected_perimeter',
+                         [
+                             (15, 706.5, 94.2),
+                         ]
+                         )
+def test_circle_creation_positive(radius, expected_area, expected_perimeter):
+    circle = Circle(radius)
+    assert circle.name == 'Circle', 'Expected name is Circle'
+    assert circle.get_area() == expected_area, 'Expected correct area'
+    assert circle.get_perimeter() == expected_perimeter, 'Expected correct perimeter'
+
+    @pytest.mark.parametrize('radius',
+                             [
+                                 0,
+                                 -2,
+                             ],
+                             ids=[
+                                 'radius is zero',
+                                 'radius is negative',
+                             ])
+    def test_circle_creation_negative(radius):
+        with pytest.raises(ValueError):
+            Circle(radius)
+
+            def test_two_circle_areas_sum():
+                expected_sum = 30
+                circle_1 = Circle(10)
+                circle_2 = Circle(2)
+                assert circle_1.add_area(circle_2) == expected_sum, f'Expected sum is {expected_sum}'
+
+@pytest.mark.parametrize('radius', [-10, 0, -50.5], ids=['negative', 'zero', 'negative_float'])
+def test_circle_creation_negative(radius):
+    with pytest.raises(ValueError):
+        Circle(radius)
+
+
